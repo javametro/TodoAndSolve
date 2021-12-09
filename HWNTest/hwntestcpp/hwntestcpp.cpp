d3bd7e9d31488837b8f83e9dfc965fde28d9b5dc
wangpeng17 <wangpeng17@lenovo.com>
2021-12-09 10:22:09 +0800

    add new project hwnttest

---
+++ HWNTest/hwntestcpp/hwntestcpp.cpp
@@ -0,0 +1,40 @@
+// hwntestcpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    LPWSTR strCommandLine = GetCommandLine();
+    std::wcout << strCommandLine << std::endl;
+
+    int nArgs;
+    LPWSTR* szArgList;
+    szArgList = CommandLineToArgvW(strCommandLine, &nArgs);
+    if (NULL == szArgList) {
+        wprintf(L"CommandLineToArgvW failed.\n");
+        return 0;
+    }else for(i=0; i<nArgs; i++)
+    
+
+}
+
+//FnLock Test
+
+//AISolutionTest
+
+//BackLight Test
+
+//
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
